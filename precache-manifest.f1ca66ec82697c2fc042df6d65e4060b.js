f1a50ce2d8a2d0fac483f5f4b8dc365ee2e90544
Joseph bayad <jobayad66@gmail.com>
2020-07-10 19:36:40 +0100

    Updates

---
+++ precache-manifest.f1ca66ec82697c2fc042df6d65e4060b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495c360a5096be0efcfc0fc69dcc96d3",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "fa7689ba767b6b89f5af",
+    "url": "/quiz-app/static/css/main.c1ee7f40.chunk.css"
+  },
+  {
+    "revision": "f34c6cd393a9cbbc1dc2",
+    "url": "/quiz-app/static/js/2.62dca63d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.62dca63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7689ba767b6b89f5af",
+    "url": "/quiz-app/static/js/main.c3a2fa05.chunk.js"
+  },
+  {
+    "revision": "5753bc72198010f821e8",
+    "url": "/quiz-app/static/js/runtime-main.15ff6bde.js"
+  }
+]);
